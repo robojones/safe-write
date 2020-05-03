3b93ad579e927a3888665d9865e898109f1b9707
Jonathan Scholz <contact@jonathan-scholz.de>
2020-05-03 22:25:09 +0200

    docs(godoc): fix list formatting (#7)

--- doc.go
+++ doc.go
@@ -29,20 +29,20 @@ there is always a consistent version of your file on the disk.
 Lets assume we are using the WriteFile method to create a file called config.json.
 The write procedure is as follows.
 
-1. Write the contents of the file to a temporary file (e.g. config.json.2020-01-02T15-04-05.000000)
-2. Create a hard link from config.json.1 to the temporary file
-3. Create a hard link from config.json to the temporary file
-4. Remove the temporary file. Because we are using hard links, the contents of the file are still available using the file names config.json and config.json.1
+	1. Write the contents of the file to a temporary file (e.g. config.json.2020-01-02T15-04-05.000000)
+	2. Create a hard link from config.json.1 to the temporary file
+	3. Create a hard link from config.json to the temporary file
+	4. Remove the temporary file. Because we are using hard links, the contents of the file are still available using the file names config.json and config.json.1
 
 The reason why the intermediate link config.json.1 is created becomes clear when we overwrite the contents of the file.
 Again, we are using the WriteFile method.
 
-1. Write the updated contents of the file to a new temporary file (e.g. config.json.2020-02-01T10-03-08.004001)
-2. Remove the previous hard link config.json.1
-3. Create a new hard link from config.json.1 to our new temporary file
-4. Remove the previous hard link config.json
-5. Create a new hard link from config.json to the new temporary file
-6. Remove the temporary file
+	1. Write the updated contents of the file to a new temporary file (e.g. config.json.2020-02-01T10-03-08.004001)
+	2. Remove the previous hard link config.json.1
+	3. Create a new hard link from config.json.1 to our new temporary file
+	4. Remove the previous hard link config.json
+	5. Create a new hard link from config.json to the new temporary file
+	6. Remove the temporary file
 
 The intermediate link config.json.1 is created so there is always one of our links config.json or config.json.1
 pointing to a complete version of our config file. Even if our process is interrupted by a server crash,
